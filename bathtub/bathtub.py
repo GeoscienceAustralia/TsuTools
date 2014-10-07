d434985301b2add6b71eb1323bfc4c30868f0557
davies <none@none.com>
2014-10-07 17:24:37 +1100

    Work-around for strange issue where NaN's appeared in stage but not depth.

--- bathtub/bathtub.py
+++ bathtub/bathtub.py
@@ -15,9 +15,9 @@ def make_bathtub_maps(stage_raster_file,
                       ocean_polygon_file,
                       ocean_polygon_file_layername,
                       output_dir='./bathtub_outputs',
-                      verbose=True,
+                      quiet=False,
                       fill_max_iterations=2000,
-                      clean_temp_files=True):
+                      keep_temp_files=False):
     """Make a 'bathtub' flood map using input stage & elevation rasters +
     an 'ocean polygon'
 
@@ -39,15 +39,18 @@ def make_bathtub_maps(stage_raster_file,
     @param ocean_polygon_file
         Polygon shapefile. Stage is clipped to this before dilating.
         For tsunami applications it would typically cover ocean areas
+    @param ocean_polygon_file_layername
+        Layer name (according to ogr) for polygon file. For a shapefile this
+        will be the file basename without .shp
     @param output_dir
         Location for output files. Created if necessary.
     @param fill_max_iterations
         The clipped data is flood-filled by at most this many pixels.
         Increase for large rasters
-    @param clean_temp_files
-        Clean up temporary files
-    @param verbose
-        Print commands passed to os.system
+    @param keep_temp_files
+        Don't clean up temporary files
+    @param quiet
+        Don't print commands passed to os.system
 
     """
 
@@ -66,7 +69,9 @@ def make_bathtub_maps(stage_raster_file,
     # Preliminary filename definition / creation
     raw_stage_copy = os.path.join(output_dir, 'raw_stage_copy.tif')
     poly_mask = os.path.join(output_dir, 'poly_mask.tif')
+    poly_mask_0nodata = os.path.join(output_dir, 'poly_mask_0nodata.tif')
     stage_clipped = os.path.join(output_dir, 'stage_clipped.tif')
+    wet_area = os.path.join(output_dir, 'wet_area.tif')
     stage_flood_filled_clipped = os.path.join(
         output_dir, 'stage_flood_filled_clipped.tif')
     depth_flood_filled_clipped = os.path.join(
@@ -81,53 +86,75 @@ def make_bathtub_maps(stage_raster_file,
     zero_raster_cmd = 'gdal_calc.py ' + '-A ' + raw_stage_copy +\
                       ' --calc=A*0.' +\
                       ' --outfile=' + poly_mask + ' --NoDataValue=0.0'
-    if verbose:
+    if not quiet:
         print zero_raster_cmd
     os.system(zero_raster_cmd)
+
+    # Convert mask nodata values to zero
+    zero_nodata_cmd = 'gdalwarp -srcnodata 0 -dstnodata 0 ' +\
+                      poly_mask + ' ' + poly_mask_0nodata
+    if not quiet:
+        print zero_nodata_cmd
+    os.system(zero_nodata_cmd)
+    ## Skip this
+    #poly_mask_0nodata = poly_mask
+    
 
     # Make a 1/0 mask defining the ocean_polygon as a raster
     polygon_burnin_cmd = 'gdal_rasterize ' + ' -at ' + ' -l ' +\
                          ocean_polygon_file_layername +\
                          ' -burn 1.0 ' +\
-                         ocean_polygon_file + ' ' + poly_mask
-    if verbose:
+                         ocean_polygon_file + ' ' + poly_mask_0nodata
+    if not quiet:
         print polygon_burnin_cmd
     os.system(polygon_burnin_cmd)
 
     # Make the 'clipped stage' file
     clip_stage_cmd = 'gdal_calc.py ' +\
                      ' -A ' + raw_stage_copy +\
-                     ' -B ' + poly_mask + ' --calc=A*B ' +\
+                     ' -B ' + poly_mask_0nodata + ' --calc=A*B ' +\
                      ' --outfile ' + stage_clipped
-    if verbose:
+    if not quiet:
         print clip_stage_cmd
     os.system(clip_stage_cmd)
 
     # Fill the clipped_stage file
     fill_stage_cmd = 'gdal_fillnodata.py ' +\
                      '-md ' + str(int(fill_max_iterations)) + ' ' +\
-                     stage_clipped + ' -mask ' + poly_mask
-    if verbose:
+                     stage_clipped + ' -mask ' + poly_mask_0nodata
+    if not quiet:
         print fill_stage_cmd
     os.system(fill_stage_cmd)
 
+    # Compute the 'wet-area' for stage/depth calculations
+    # Doing this separately from the final 2 computations seems to remove 
+    # minor NaN artefacts which otherwise appear there
+    wet_area_cmd = 'gdal_calc.py ' + ' -A ' + stage_clipped +\
+                   ' -B ' + elevation_raster_file + ' --calc="(B<A)"' +\
+                   ' --outfile ' + wet_area
+    if not quiet:
+        print wet_area_cmd
+    os.system(wet_area_cmd)
+
     # Clip the filled stage based on the elevation
     clip_stage_cmd = 'gdal_calc.py ' + ' -A ' + stage_clipped +\
-                     ' -B ' + elevation_raster_file + ' --calc="A*(B<A)"' +\
+                     ' -B ' + elevation_raster_file +\
+                     ' -C ' + wet_area + ' --calc="A*C"' +\
                      ' --outfile ' + stage_flood_filled_clipped
-    if verbose:
+    if not quiet:
         print clip_stage_cmd
     os.system(clip_stage_cmd)
 
     # Convert to depth
     depth_cmd = 'gdal_calc.py ' + ' -A ' + stage_flood_filled_clipped +\
-                ' -B ' + elevation_raster_file + ' --calc="(A-B)*(B<A)"' +\
+                ' -B ' + elevation_raster_file +\
+                ' -C ' + wet_area + ' --calc="(A-B)*C"' +\
                 ' --outfile ' + depth_flood_filled_clipped
-    if verbose:
+    if not quiet:
         print depth_cmd
     os.system(depth_cmd)
 
-    if clean_temp_files:
+    if not keep_temp_files:
         for filename in temp_files:
             os.remove(filename)
 
@@ -154,13 +181,13 @@ if __name__ == '__main__':
     parser.add_argument('-fill_max_iterations', type=int, default=2000,
                         help='The clipped data is flood-filled by at most ' +
                              'this many pixels. Increase for large rasters')
-    parser.add_argument('-clean_temp_files', type=bool, default=True,
+    parser.add_argument('-keep_temp_files', action='store_true', default=False,
                         help='Clean up temporary files')
-    parser.add_argument('-verbose', type=bool, default=True,
+    parser.add_argument('-quiet', action='store_true', default=False,
                         help='Print commands passed to os.system')
 
     args = parser.parse_args()
-
+    
     ocean_polygon_file_layername = os.path.splitext(os.path.basename(
         args.ocean_polygon_file))[0]
 
@@ -169,6 +196,6 @@ if __name__ == '__main__':
                       args.ocean_polygon_file,
                       ocean_polygon_file_layername,
                       args.output_dir,
-                      args.verbose,
+                      args.quiet,
                       args.fill_max_iterations,
-                      args.clean_temp_files)
+                      args.keep_temp_files)
