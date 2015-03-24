c4100b9c430ee157c7669c9b0039274c8b6cb852
Jonathan Griffin <jdg547@raijin2.(none)>
2015-03-24 12:40:07 +1100

    Added functionality to return plus/minus one sigma values as well as mean values, and test

--- scaling/strasser2010.py
+++ scaling/strasser2010.py
@@ -11,7 +11,7 @@ Jonathan Griffin
 Geoscience Australia
 October 2014
 """
-
+import sys
 import numpy
 
 def calculate_dimensions(magnitude):
@@ -24,14 +24,31 @@ def calculate_dimensions(magnitude):
     coefficients = {'length': [-2.477, 0.585],
                     'width': [-0.882, 0.351],
                     'area': [-3.476, 0.952]}
+    sigmas = {'length': 0.180,
+              'width': 0.173,
+              'area': 0.304}
     length = numpy.power(10, (coefficients['length'][0] + \
                                   coefficients['length'][1]*magnitude)) 
     width = numpy.power(10, (coefficients['width'][0] + \
                                   coefficients['width'][1]*magnitude))
     area = numpy.power(10, (coefficients['area'][0] + \
                                   coefficients['area'][1]*magnitude))
-
-    return length, width, area
+    sigma_length_plusone = numpy.power(10, (coefficients['length'][0] + \
+                           coefficients['length'][1]*magnitude) + sigmas['length']) 
+    sigma_width_plusone = numpy.power(10, (coefficients['width'][0] + \
+                           coefficients['width'][1]*magnitude) + sigmas['width'])
+    sigma_area_plusone = numpy.power(10, (coefficients['area'][0] + \
+                           coefficients['area'][1]*magnitude) + sigmas['area'])
+    sigma_length_minusone = numpy.power(10, (coefficients['length'][0] + \
+                           coefficients['length'][1]*magnitude) - sigmas['length'])
+    sigma_width_minusone = numpy.power(10, (coefficients['width'][0] + \
+                           coefficients['width'][1]*magnitude) - sigmas['width'])
+    sigma_area_minusone = numpy.power(10, (coefficients['area'][0] + \
+                           coefficients['area'][1]*magnitude) - sigmas['area'])
+    return length, width, area, sigma_length_plusone, \
+        sigma_width_plusone, sigma_area_plusone, \
+        sigma_length_minusone, sigma_width_minusone, \
+        sigma_area_minusone
 
 def calculate_magnitude(dimension_type, dimension_value):
     """
@@ -51,3 +68,27 @@ def calculate_magnitude(dimension_type, dimension_value):
 
     return magnitude
 
+if __name__ == "__main__":
+    def usage():
+        print 'Usage: python strasser2010.py <dimension_type> <dimension_value>'
+        print 'dimension_type: one of "magnitude", "length", "width", "area"'
+        print 'dimension_value: value of input dimension in Mw, km, km, km^2'
+
+    try:
+        dimension_type = sys.argv[1]
+        dimension_value = float(sys.argv[2])
+    except:
+        usage()
+        sys.exit()
+    if dimension_type == 'magnitude':
+        length, width, area = calculate_dimensions(dimension_value)
+        print 'Length (km):', length
+        print 'Width (km):', width
+        print 'Area (km):', area
+    elif dimension_type == 'length' or dimension_type == 'width' \
+            or dimension_type == 'area':
+        magnitude = calculate_magnitude(dimension_type, dimension_value)
+        print 'Magnitude (Mw):', magnitude
+    else:
+        usage()
+        sys.exit()
--- scaling/test_strasser2010.py
+++ scaling/test_strasser2010.py
@@ -18,10 +18,25 @@ class Test_strasser2010(unittest.TestCase):
                      'length': [41.4954042634, 159.5879147237, 702.2633437358],
                      'width': [37.5837404288, 84.3334757764, 205.1634530751],
                      'area': [1541.7004529496, 13803.8426460288, 
-                            153886.3149819950]}
+                            153886.3149819950],
+                     'sigma_length_plusone': [62.80583588, 241.5460834, \
+                                                  1062.918583],
+                     'sigma_width_plusone': [55.97576015, 125.6029964, \
+                                                 305.5624616],
+                     'sigma_area_plusone': [3104.559588, 27797.13268, \
+                                                309884.6042],
+                     'sigma_length_minusone': [27.41574172, 105.4386896, \
+                                                   463.98079],
+                     'sigma_width_minusone': [25.23480772, 56.6239289, \
+                                                  137.752662],
+                     'sigma_area_minusone': [765.5966069, 6854.882265, \
+                                                 76418.7624]}
+
         i = 0
         for magnitude in test_data['magnitude']:
-            length, width, area = calculate_dimensions(magnitude)
+            length, width, area, sigma_length_plusone, sigma_width_plusone, \
+            sigma_area_plusone, sigma_length_minusone, sigma_width_minusone, \
+            sigma_area_minusone = calculate_dimensions(magnitude)
             msg = 'Expected value of %.10f, got %.10f for length for \
                    magnitude %.1f' % (test_data['length'][i], length, magnitude)
             assert numpy.allclose(test_data['length'][i], length, rtol=1.e-9),\
@@ -33,6 +48,39 @@ class Test_strasser2010(unittest.TestCase):
             msg = 'Expected value of %.10f, got %.10f for area for \
                    magnitude %.1f' % (test_data['area'][i], area, magnitude)
             assert numpy.allclose(test_data['area'][i], area, rtol=1.e-9), msg
+
+            msg = 'Expected value of %.10f, got %.10f for length + 1 sigma for \
+                   magnitude %.1f' % (test_data['sigma_length_plusone'][i], \
+                                          sigma_length_plusone, magnitude)
+            assert numpy.allclose(test_data['sigma_length_plusone'][i], \
+                                      sigma_length_plusone, rtol=1.e-9), msg
+            msg = 'Expected value of %.10f, got %.10f for width + 1 sigma for \
+                   magnitude %.1f' % (test_data['sigma_width_plusone'][i], \
+                                          sigma_width_plusone, magnitude)
+            assert numpy.allclose(test_data['sigma_width_plusone'][i], \
+                                      sigma_width_plusone, rtol=1.e-9), msg
+            msg = 'Expected value of %.10f, got %.10f for area + 1 sigma for \
+                   magnitude %.1f' % (test_data['sigma_area_plusone'][i], \
+                                          sigma_area_plusone, magnitude)
+            assert numpy.allclose(test_data['sigma_area_plusone'][i], \
+                                      sigma_area_plusone, rtol=1.e-9), msg
+
+            msg = 'Expected value of %.10f, got %.10f for length + 1 sigma for \
+                   magnitude %.1f' % (test_data['sigma_length_minusone'][i], \
+                                          sigma_length_minusone, magnitude)
+            assert numpy.allclose(test_data['sigma_length_minusone'][i], \
+                                      sigma_length_minusone, rtol=1.e-9), msg
+            msg = 'Expected value of %.10f, got %.10f for width + 1 sigma for \
+                   magnitude %.1f' % (test_data['sigma_width_plusone'][i], \
+                                          sigma_width_minusone, magnitude)
+            assert numpy.allclose(test_data['sigma_width_minusone'][i], \
+                                      sigma_width_minusone, rtol=1.e-9), msg
+            msg = 'Expected value of %.10f, got %.10f for area + 1 sigma for \
+                   magnitude %.1f' % (test_data['sigma_area_minusone'][i], \
+                                          sigma_area_minusone, magnitude)
+            assert numpy.allclose(test_data['sigma_area_minusone'][i], \
+                                      sigma_area_minusone, rtol=1.e-9), msg
+
             i+=1
 
     def test_calculate_magnitude(self):
